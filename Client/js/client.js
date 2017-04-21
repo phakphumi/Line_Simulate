23e88c922411e7e4b6b1ea45822892f3e2cdf389
Thuchchai Jiamsorn <naki_war@windowslive.com>
2017-04-22 06:59:56 +0700

    server v.1 done

--- Client/js/client.js
+++ Client/js/client.js
@@ -1,46 +1,112 @@
 var socket;
+var thisUser;
 function message(msg){
-	$('#chatLog').append(msg+'</p>');
+    var chatLog = $('#chatLog');
+    var bottom = false;
+    if((chatLog[0].scrollHeight-chatLog[0].clientHeight) - chatLog.scrollTop() < 30)
+        bottom = true;
+    chatLog.append(msg+'</p>');
+    if(bottom)
+        chatLog.scrollTop(chatLog[0].scrollHeight);
 }
-function sendBtn() {
-    var send = function() {
-        var text = $('#text').val();
-        socket.send(text);
-        $('#text').val("");
+function socketInput(Status) {
+    if(Status)
+        $('.input-socket,.input-btn').prop('disabled',false);
+    else
+        $('.input-socket,.input-btn').prop('disabled',true);
+}
+function sendSocket(msg,type,opt) {
+    var type = arguments.length <= 1 ? 'msg' : type;
+    var opt = arguments.length <= 2 ? '' : opt;
+    var obj = {
+        type : type,
+        msg : msg,
+        opt: opt
+    };
+    socket.send(JSON.stringify(obj));
+}
+function setupSocketInput() {
+    var input = $('.input-socket');
+    var input_group = input.parents('.input-group');
+    var btn = input_group.find('.input-btn');
+    var sendInput = function(input) {
+        var text = input.val();
+        var type = input.attr('data-type');
+        input.val("");
+        sendSocket(text,type);
     }
-    $('#send').on('click',function(){
-        send();
+    btn.off('click.socket').on('click.socket',function(e){
+        var input = $(this).parents('.input-group').find('.input-socket');
+        sendInput(input);
     });
-    $('#text').keypress(function(e) {
-        if(e.which == 13){
-            send();
-        }
-    })
+    input.off('keypress.socket').on('keypress.socket',function(e) {
+        if(e.which == 13)
+            sendInput($(this));
+    });
 }
 function initAfterConn() {
-    sendBtn();
+    setupSocketInput();
+}
+function setUserName() {
+    if(thisUser == null){
+        iniUser();
+        return;
+    }
+    sendSocket(thisUser,'user',{newLogin:false});
 }
 function connect(){
     try{
-	var host = "ws://localhost:8081/";
-	// var host = "ws://192.168.43.245:8081/";//192.168.43.39
-    socket = new WebSocket(host);
+    	// var host = "ws://localhost:8081/";
+    	var host = "ws://192.168.137.1:8081/";//192.168.43.39//192.168.137.1
+        socket = new WebSocket(host);
 
-        message('<p class="event">Socket Status: '+socket.readyState);
+
+        // message('<p class="event">Socket Status: '+socket.readyState);
 
         socket.onopen = function(){
             console.log(socket.readyState+' (open)');
-            message('<p class="event"><span class="word-received">Socket Status:</span> '+socket.readyState+' (open)');
+            socketInput(true);
+            $('.connecting').removeClass('connecting');
+            $('#server').html('<p class="event"><span class="word-received">Socket Status:</span> Connected</p>');
+            setUserName();
         }
 
-        socket.onmessage = function(msg){
-            console.log(msg.data);
-            message('<p class="message"><span class="word-received">Received:</span> '+msg.data);
+        socket.onmessage = function(res){
+            console.log(res.data);
+            var res = JSON.parse(res.data);
+            var user = res.user;
+            var msg = res.msg;
+            switch (res.type) {
+                case 'msg':
+                    if(user == thisUser)
+                        user = 'You ('+thisUser+')';
+                    message('<p class="message"><span class="word-received">'+user+' :</span> '+ msg );
+                    break;
+                case 'log':
+                    console.log(msg);
+                    break;
+                case 'success':
+                    $.each(BootstrapDialog.dialogs, function(id, dialog){
+                        dialog.close();
+                    });
+                    if(thisUser == null){
+                        thisUser = msg;
+                        BootstrapDialog.show({
+                            message:    '<h1> Hello, '+msg+' :D</h1>',
+                        });
+                    }
+
+                default:
+                    break;
+            }
         }
 
         socket.onclose = function(){
             console.log(socket.readyState+' (Closed)');
-            message('<p class="event"><span class="word-received">Socket Status:</span> '+socket.readyState+' (Closed)');
+            socketInput(false);
+            if($('.connecting').length==0)
+                $('#server').html('<p class="event connecting"><span class="word-received">Socket Status:</span> Connecting....</p>');
+            setTimeout(connect(),1000);
         }
 
         initAfterConn();
@@ -54,8 +120,24 @@ function testGet() {
         console.log( data );
     });
 }
-
+function iniUser() {
+    var newE = $('#input-template').find('.input-group').clone();
+    newE.find('.input-socket').attr('data-type','user');
+    BootstrapDialog.show({
+        title: 'Enter your name',
+        closeByKeyboard: true,
+        message: function(){
+            var dialog = $('<div class="row"></div>').html('<div class="col-sm-6 item"></div>');
+            dialog.find('.item').html(newE);
+            return dialog;
+        },
+        onshown : function(dialog) {
+            setupSocketInput();
+        }
+    });
+}
 $(function() {
+    // socketInput(false);
     // testGet();
     connect();
 });
--- Node/chatserver.js
+++ Node/chatserver.js
@@ -1,26 +1,105 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 8081 });
 
-
+WebSocket.prototype.user = 'Guess';
+WebSocket.prototype.sendSocket = function(res) {
+    var obj = {
+        type : 'msg',
+        user: 'Server',
+        msg : '...',
+        opt: '',
+    };
+    Object.assign(obj,res);
+    this.send(JSON.stringify(obj));
+}
+var roomList = {
+    room : {
+        userList : {
+            user : {
+                unread : [0,1]
+            },
+        },
+        chatsList : ['obj'],
+    },
+}
+function sendUnread(ws) {
+    var user = ws.user;
+    if((unreads = roomList[ws.room].userList[user].unread) != null){
+        roomList[ws.room].userList[user].unread = [];
+        console.log(unreads);
+        for (var i = 0; i < unreads.length; i++) {
+            ws.sendSocket(unreads[i]);
+        }
+    }
+}
 wss.on('connection', function connection(ws) {
+    ws.sendSocket({msg:'Greeting :D',type:'log'});
     ws.on('message', function incoming(data) {
-        ws.send('You : '+data);
-        // Broadcast to everyone else.
-        wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
-    });
-    ws.send('Hey You :D');
-});
-
-wss.broadcast = function broadcast(data) {
-    console.log(data);
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
+        data = JSON.parse(data);
+        console.log(ws.user,' : ',data);
+        var msg = data.msg;
+        switch (data.type) {
+            case 'msg':
+                var user = ws.user;
+                wss.broadcast({msg:msg,user:user},ws);
+                break;
+            case 'user':
+                var user = msg;
+                ws.user = user;
+                ws.room = 'room';
+                if(roomList[ws.room].userList[user] == null)
+                    roomList[ws.room].userList[user] = {};
+                console.log(user,'logged in');
+                ws.sendSocket({msg:msg,user:'You are ',type:'success'});
+                if(data.opt.newLogin == null || (data.opt.newLogin != null && data.opt.newLogin)){
+                    wss.broadcast({msg:ws.user+' joined'});
+                }
+                sendUnread(ws);
+                break;
+            case 'debug':
+                console.log(userList);
+                console.log(groupList);
+            default:
         }
     });
+    ws.on('close',function close() {
+        console.log(ws.user,'logged out');
+        wss.broadcast({msg:ws.user+' logged out'});
+    });
+});
+
+wss.broadcast = function broadcast(data,ws) {
+    var room = 'room';
+    if(ws != null) room = ws.room;
+    function online() {
+        var onlineList = [];
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN && client.user != null) {
+                if(client.user != 'Guess' && client.room == room){
+                    onlineList.push(client.user);
+                    client.sendSocket(data);
+                }
+            }
+        });
+        return onlineList;
+    }
+    function offline(obj) {
+        var onlineList = obj.onlineList;
+        var userList = roomList[room].userList;
+        Object.keys(userList).forEach(function(user) {
+            if(onlineList.indexOf(user)>=0)
+                return true;
+            console.log(user);
+            if(userList[user].unread == null)
+                userList[user].unread = [data]
+            else
+                userList[user].unread.push(data);
+        });
+        // roomList[room].userList = userList;
+        console.log('onlineList',onlineList);
+        console.log('userList',userList);
+    }
+
+    var onlineList = online();
+    offline({onlineList:onlineList});
 };
