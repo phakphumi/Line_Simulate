7f40e98fe358f33d58745dc49a9caec1d76f8368
Thuchchai Jiamsorn <naki_war@windowslive.com>
2017-04-21 22:22:37 +0700

    update client ui

--- Client/js/client.js
+++ Client/js/client.js
@@ -3,11 +3,19 @@ function message(msg){
 	$('#chatLog').append(msg+'</p>');
 }
 function sendBtn() {
-    $('#send').on('click',function send(){
+    var send = function() {
         var text = $('#text').val();
         socket.send(text);
         $('#text').val("");
+    }
+    $('#send').on('click',function send(){
+        send();
     });
+    $('#text').keypress(function(e) {
+        if(e.which == 13){
+            send();
+        }
+    })
 }
 function initAfterConn() {
     sendBtn();
@@ -22,17 +30,17 @@ function connect(){
 
         socket.onopen = function(){
             console.log(socket.readyState+' (open)');
-            message('<p class="event">Socket Status: '+socket.readyState+' (open)');
+            message('<p class="event"><span class="word-received">Socket Status:</span> '+socket.readyState+' (open)');
         }
 
         socket.onmessage = function(msg){
             console.log(msg.data);
-            message('<p class="message">Received: '+msg.data);
+            message('<p class="message"><span class="word-received">Received:</span> '+msg.data);
         }
 
         socket.onclose = function(){
             console.log(socket.readyState+' (Closed)');
-            message('<p class="event">Socket Status: '+socket.readyState+' (Closed)');
+            message('<p class="event"><span class="word-received">Socket Status:</span> '+socket.readyState+' (Closed)');
         }
 
         initAfterConn();
