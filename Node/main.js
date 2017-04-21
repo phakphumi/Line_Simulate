251211c828dd906aae2c577ca85c9539aabd9948
Thuchchai Jiamsorn <naki_war@windowslive.com>
2017-04-21 20:38:24 +0700

    add client and install ws on nodejs

--- Node/main.js
+++ Node/main.js
@@ -1,15 +1,13 @@
-var http = require("http");
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, response) {
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
 
-   // Send the HTTP header
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
 
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+   console.log("Example app listening at http://%s:%s", host, port)
+})
