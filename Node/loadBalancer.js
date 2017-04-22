0c20d95d24dfc3cb68811249e6b03f15ae8b6829
Joe_Pakpoom <pakpoom.thawee@gmail.com>
2017-04-22 07:58:12 +0700

    no message

--- Node/loadBalancer.js
+++ Node/loadBalancer.js
@@ -1,9 +1,28 @@
 var WebSocket = require('ws');
+var express = require('express');
+var app = express();
 
-function initWS(port){
-    console.log(port);
-    var ws = port === 8081 ? new WebSocket('ws://localhost:8081') 
-                            : new WebSocket('ws://localhost:8082'); 
+var ip = ["127.0.0.1", "127.0.0.1"];
+var port = ["8081", "8082"];
+
+var chosenIP = ip[0];
+var chosenPort = port[0];
+
+app.get('/online', function(req, res) {
+    // res.send('About us');
+    var onlineServer = {'ip': chosenIP, 'port': chosenPort};
+    
+    res.send(JSON.stringify(onlineServer));
+
+});
+
+app.listen(3030, function () {
+    console.log('Express Server started on port ' + 3030 +'!');
+});
+
+function initWS(){
+
+    var ws = new WebSocket('ws://'.concat(chosenIP).concat(':').concat(chosenPort)); 
 
     ws.on('error', function(err) {
         console.log('Waiting For Connection..');
@@ -12,89 +31,22 @@ function initWS(port){
 
     ws.on('open', function() {
 
-        console.log('Server on port' + port + 'started');    
+        console.log('Server on ' + chosenIP + ':' + chosenPort + ' is online');    
 
     });
 
     ws.on('close', function() {
 
-        if(port === 8081) {
-
-            port = 8082;
-
-        } else {
-
-            port = 8081;
-
-        }
         console.log('Server was closed');
-        setTimeout(initWS, 1000, port);
+
+        // toggle server
+        chosenIP = chosenIP === ip[0] ? ip[1] : ip[0];
+        chosenPort = chosenPort === port[0] ? port[1] : port[0];
+        
+        setTimeout(initWS, 1000);
 
     });
 
 }
 
-initWS(8081);
-// ws.on('close', function() {
-
-//     if(port === 8081) {
-
-//         port = 8082;
-//         console.log(port);
-
-//     } else {
-
-//         port = 8081;
-//         console.log(port);
-
-//     }
-
-//     console.log('Waiting For Connection..');
-
-//     // setTimeout(poll, 1000);
-
-// })
-
-
-// // var poll = function( ) {
-
-// //     if(port === 8081) {
-
-// //         ws = new WebSocket('ws://localhost:8082');
-// //         port = 8082;
-
-// //     } else {
-
-// //         ws = new WebSocket('ws://localhost:8081');
-// //         port = 8081;
-
-// //     }
-
-// //     ws.on('open', function() {
-
-// //         if(port === 8081) {
-
-// //             console.log('Server on port 8081 started');
-
-// //         } else {
-
-// //             console.log('Server on port 8082 started');
-
-// //         }
-        
-// //     });
-
-// //     ws.on('message', function(message) {
-// //         console.log(message);
-// //     });
-
-// //     ws.on('error', function(err) {
-// //         // Check error code? Maybe put it in close event?
-// //         // setTimeout( poll, 1000);
-// //         console.log('Waiting For Connection..');
-// //     });
-
-// //     ws.on('close', function() {
-// //         console.log("inclose");
-// //     });
-// }
+initWS();
